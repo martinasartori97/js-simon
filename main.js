f38b0bcccfaf5b8fa9a87c82edb12f8e8b02da7a
martinasartori97 <martinasartori9@gmail.com>
2024-10-10 14:28:20 +0200

    add comments

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 console.log("simon Says");
+
+
+//creo in pagina 5 numeri casuali.
+
+// faccio partire un timer di 30 secondi.
+
+//faccio scomparire i numeri dopo 30 secondi e faccio apparire  5 input in cui l'utente deve inserire i numeri che ha visto precedentemente, nell'ordine che preferisce.
+
+//Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
